98ff924384ebe1a3f3953730b3bc654a14c5c5e0
Jean <oka.jeanjaures@gmail.com>
2024-09-03 19:52:21 +0200

    refactor

--- src/main/java/com/example/paris_janitor_api/repository/UserRepository.java
+++ src/main/java/com/example/paris_janitor_api/repository/UserRepository.java
@@ -1,33 +1,16 @@
 package com.example.paris_janitor_api.repository;
 
-import com.example.paris_janitor_api.dto.UserDto;
 import com.example.paris_janitor_api.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
+import java.util.Optional;
 
-//@Repository
+@Repository
 public interface UserRepository extends MongoRepository<User,String> {
-
-    List<User> findAll();
-
-    User findUserByEmail(String email);
-
-    User findUserById(String id);
-
-    @Override
-    <S extends User> S insert(S entity);
-
-    @Override
-    <S extends User> S save(S entity);
-
-    @Override
-    void deleteById(String id);
-
-    //void updateUserByEmail(String id,UserDto userDto);
-
-    //void updateUserById()
-    //void updateUserById(String id, UserDto userDto);
-
+    boolean existsUserByEmail(String login);
+    boolean existsUserByUsername(String username);
+    Optional<User> findUserByUsername(String username);
+    //User findUserByUsername(String username);
+    Optional<User> findUserByUsernameAndPassword(String username,String password);
 }
