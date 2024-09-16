0c2db4c574a57aee5145c95c1446983345ca313c
Jean <oka.jeanjaures@gmail.com>
2024-09-17 01:50:35 +0200

    refactor : add annotations

--- src/main/java/com/example/paris_janitor_api/repository/BookingRepository.java
+++ src/main/java/com/example/paris_janitor_api/repository/BookingRepository.java
@@ -1,4 +1,10 @@
 package com.example.paris_janitor_api.repository;
 
-public interface BookingRepository {
+
+import com.example.paris_janitor_api.model.Booking;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookingRepository extends MongoRepository<Booking, String> {
 }
--- src/main/java/com/example/paris_janitor_api/repository/DeliveryRepository.java
+++ src/main/java/com/example/paris_janitor_api/repository/DeliveryRepository.java
@@ -1,9 +1,10 @@
 package com.example.paris_janitor_api.repository;
 
+import com.example.paris_janitor_api.model.Booking;
 import com.example.paris_janitor_api.model.Delivery;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DeliveryRepository extends MongoRepository<Delivery,String> {
+public interface DeliveryRepository extends MongoRepository<Delivery, String>{
 }
