5b4887f77d6500137953947f95afb0f6b0ceacbd
Jean <oka.jeanjaures@gmail.com>
2024-09-17 01:51:43 +0200

    refactor : add annotations to handle current date

--- src/main/java/com/example/paris_janitor_api/config/MongoConfig.java
+++ src/main/java/com/example/paris_janitor_api/config/MongoConfig.java
@@ -4,10 +4,13 @@ import com.mongodb.client.MongoClients;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+
 @Configuration
+@EnableMongoAuditing
 @EnableMongoRepositories(basePackages = "com.example.paris_janitor_api.repository")
 public class MongoConfig {
 
