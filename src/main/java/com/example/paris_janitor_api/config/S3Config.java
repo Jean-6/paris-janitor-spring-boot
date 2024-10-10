caebc2bae25e35975cce36371bf280e4a1b915ff
Jean <oka.jeanjaures@gmail.com>
2024-10-10 02:39:09 +0200

    fix: config aws credentials properly

--- src/main/java/com/example/paris_janitor_api/config/S3Config.java
+++ src/main/java/com/example/paris_janitor_api/config/S3Config.java
@@ -3,7 +3,8 @@ package com.example.paris_janitor_api.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import software.amazon.awssdk.auth.credentials.DefaultCredentialsProvider;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.S3Client;
 
@@ -11,14 +12,23 @@ import software.amazon.awssdk.services.s3.S3Client;
 @Configuration
 public class S3Config {
 
-    @Value("${aws.region.name}")
-    private String regionName;
+    @Value("${aws.access.key}")
+    private String accessKey;
+
+    @Value("${aws.secret.key}")
+    private String secretKey;
+
+    //@Value("${aws.region.name}")
+    //private String regionName;
 
     @Bean
     public S3Client s3Client() {
+        StaticCredentialsProvider credentialsProvider = StaticCredentialsProvider.create(
+                AwsBasicCredentials.create(accessKey, secretKey)
+        );
         return S3Client.builder()
                 .region(Region.of("eu-north-1"))
-                .credentialsProvider(DefaultCredentialsProvider.create())
+                .credentialsProvider(credentialsProvider)
                 .build();
     }
 }
