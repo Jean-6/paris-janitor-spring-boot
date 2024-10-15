31ff650db963ff544b676ae8384a48009cb3b343
Jean <oka.jeanjaures@gmail.com>
2024-10-15 14:13:11 +0200

    refactor: web Mvc Configurer

--- src/main/java/com/example/paris_janitor_api/config/WebConfig.java
+++ src/main/java/com/example/paris_janitor_api/config/WebConfig.java
@@ -2,16 +2,19 @@ package com.example.paris_janitor_api.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 @Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Apply to all paths
                 .allowedOrigins("http://localhost:4200") // Allow requests from this origin
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow these HTTP methods
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH") // Allow these HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials (cookies, authorization headers, etc.)
     }
