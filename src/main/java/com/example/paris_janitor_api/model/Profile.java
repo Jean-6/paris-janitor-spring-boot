c0b5b2aad1faf1c77d21e625fbe10227f20019f3
Jean <oka.jeanjaures@gmail.com>
2024-10-15 14:16:58 +0200

    refactor : redefine model by adding new field

---
+++ src/main/java/com/example/paris_janitor_api/model/Profile.java
@@ -0,0 +1,16 @@
+package com.example.paris_janitor_api.model;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Profile {
+    private String username;
+    private String email;
+}
--- src/main/java/com/example/paris_janitor_api/model/Property.java
+++ src/main/java/com/example/paris_janitor_api/model/Property.java
@@ -29,6 +29,7 @@ public class Property {
     private Address address;
     @CreatedBy
     private String userId;
+    private String tel;
     @CreatedDate
     @JsonFormat(pattern="dd-MM-yyyy HH:mm:ss")
     private LocalDateTime createdAt;
