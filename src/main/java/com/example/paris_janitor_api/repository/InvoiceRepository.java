624ca297c285cb22610ecc36ddf0e1da0a2f38f3
Jean <oka.jeanjaures@gmail.com>
2025-02-17 02:06:25 +0100

    refactor (features): switch to another ms

--- src/main/java/com/example/paris_janitor_api/repository/InvoiceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.paris_janitor_api.repository;
-
-import com.example.paris_janitor_api.model.Invoice;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InvoiceRepository extends MongoRepository<Invoice, String> {
-}
