d3fdca66d5e96709a7b458ee21a18aad1d84d9b8
Jean <oka.jeanjaures@gmail.com>
2025-02-19 01:55:19 +0100

    refactor (files): migrate to clean architecture

--- src/main/java/com/example/paris_janitor_api/framework/db/DeliveryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.paris_janitor_api.framework.db;
-import com.example.paris_janitor_api.framework.entity.Delivery;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DeliveryRepository extends MongoRepository<Delivery, String>{
-}
