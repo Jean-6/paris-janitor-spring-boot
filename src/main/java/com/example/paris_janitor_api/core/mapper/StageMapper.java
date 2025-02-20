b9002de6449b1f434c52fac6a94452d0a80bb571
Jean <oka.jeanjaures@gmail.com>
2025-02-20 01:11:52 +0100

    refactor (infra): update mapper

--- src/main/java/com/example/paris_janitor_api/core/mapper/StageMapper.java
+++ src/main/java/com/example/paris_janitor_api/core/mapper/StageMapper.java
@@ -5,7 +5,8 @@ import com.example.paris_janitor_api.core.model.Stage;
 import com.example.paris_janitor_api.framework.db.StageDocument;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Component
 public class StageMapper {
@@ -18,28 +19,25 @@ public class StageMapper {
     }
 
     public Stage toModel(StageDocument stageDocument) {
-        if (stageDocument == null) {
-            return null;
-        }
+        if (stageDocument == null)return null;
+
         return new Stage(
                 stageDocument.getStatus(),
                 stageDocument.getCreatedAt()
         );
     }
 
-    public StageDocument[] toDocumentArray(Stage[] stages) {
+    public List<StageDocument> toDocumentArray(List<Stage> stages) {
         if(stages == null) return null;
-        return Arrays.stream(stages)
+        return stages.stream()
                 .map(this::toDocument)
-                .toArray(StageDocument[]::new);
+                .collect(Collectors.toList());
     }
 
-
-
-    public Stage[] toModelArray(StageDocument[] stageDocuments) {
+    public List<Stage> toModelArray(List<StageDocument> stageDocuments) {
         if(stageDocuments == null) return null;
-        return Arrays.stream(stageDocuments)
+        return stageDocuments.stream()
                 .map(this::toModel)
-                .toArray(Stage[]::new);
+                .collect(Collectors.toList());
     }
 }
