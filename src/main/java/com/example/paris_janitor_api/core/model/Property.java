c235b09d1471539ed0ad30c5c837b7dc3a551c3f
Jean <oka.jeanjaures@gmail.com>
2025-03-17 20:59:41 +0100

    refactor: add lombok annotation

--- src/main/java/com/example/paris_janitor_api/core/model/Property.java
+++ src/main/java/com/example/paris_janitor_api/core/model/Property.java
@@ -2,12 +2,13 @@ package com.example.paris_janitor_api.core.model;
 
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.time.LocalDateTime;
 
-
+@Data
 @Document(collection = "property")
 public class Property {
     @Id
@@ -23,58 +24,4 @@ public class Property {
     @JsonFormat(pattern="dd-MM-yyyy HH:mm:ss")
     private LocalDateTime createdAt;
     private Status status;
-
-
-    public Property(String id, String type, double area, long pieces, double rent, String description, Address address, String userId, String tel, LocalDateTime createdAt) {
-        this.id = id;
-        this.type = type;
-        this.area = area;
-        this.pieces = pieces;
-        this.rent = rent;
-        this.description = description;
-        this.address = address;
-        this.userId = userId;
-        this.tel = tel;
-        this.createdAt = createdAt;
-    }
-
-    /*public String getId() {
-        return id;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public double getArea() {
-        return area;
-    }
-
-    public long getPieces() {
-        return pieces;
-    }
-
-    public double getRent() {
-        return rent;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public Address getAddress() {
-        return address;
-    }
-
-    public String getUserId() {
-        return userId;
-    }
-
-    public String getTel() {
-        return tel;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }*/
 }
