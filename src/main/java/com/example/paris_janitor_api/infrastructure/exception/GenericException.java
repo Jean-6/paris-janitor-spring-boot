5d2880ea27e8f953a07db647d759a625e579091f
Jean <oka.jeanjaures@gmail.com>
2025-03-17 23:53:23 +0100

    refactor: add new exception

---
+++ src/main/java/com/example/paris_janitor_api/infrastructure/exception/GenericException.java
@@ -0,0 +1,7 @@
+package com.example.paris_janitor_api.infrastructure.exception;
+
+public class GenericException extends RuntimeException {
+    public GenericException(String message) {
+        super(message);
+    }
+}
