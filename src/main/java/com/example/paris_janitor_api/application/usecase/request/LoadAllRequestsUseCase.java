3a48d2e5a0ea287414c8881cd94449f0c55635c1
Jean <oka.jeanjaures@gmail.com>
2025-03-17 23:59:58 +0100

    refactor: redefine methods into use case

---
+++ src/main/java/com/example/paris_janitor_api/application/usecase/request/LoadAllRequestsUseCase.java
@@ -0,0 +1,21 @@
+package com.example.paris_janitor_api.application.usecase.request;
+
+import com.example.paris_janitor_api.application.port.in.request.LoadAllRequestsPort;
+import com.example.paris_janitor_api.core.model.Request;
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+
+@Service
+public class LoadAllRequestsUseCase implements LoadAllRequestsPort {
+
+    private final com.example.paris_janitor_api.application.port.out.request.LoadAllRequestsPort loadAllRequestsPort;
+
+    public LoadAllRequestsUseCase(com.example.paris_janitor_api.application.port.out.request.LoadAllRequestsPort loadAllRequestsPort) {
+        this.loadAllRequestsPort = loadAllRequestsPort;
+    }
+
+    @Override
+    public Flux<Request> findAll() {
+        return loadAllRequestsPort.findAll();
+    }
+}
