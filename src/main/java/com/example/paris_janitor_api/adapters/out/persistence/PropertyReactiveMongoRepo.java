2d70c77adcf04229e69b5fac28f35e3a1b2a1bc8
Jean <oka.jeanjaures@gmail.com>
2025-03-21 22:11:37 +0100

    config: add annotation to mark like data access layer

--- src/main/java/com/example/paris_janitor_api/adapters/out/persistence/PropertyReactiveMongoRepo.java
+++ src/main/java/com/example/paris_janitor_api/adapters/out/persistence/PropertyReactiveMongoRepo.java
@@ -2,6 +2,8 @@ package com.example.paris_janitor_api.adapters.out.persistence;
 
 import com.example.paris_janitor_api.core.model.Property;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PropertyReactiveMongoRepo extends ReactiveMongoRepository<Property, String> {
 }
