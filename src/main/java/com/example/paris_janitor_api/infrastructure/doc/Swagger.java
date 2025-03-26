b3b2d31e4de2dabd4b33a2a75ac4cdd0313348ca
Jean <oka.jeanjaures@gmail.com>
2025-03-26 23:56:08 +0100

    config: add springdoc webflux ui as dependency and set swagger params

--- src/main/java/com/example/paris_janitor_api/infrastructure/doc/Swagger.java
+++ src/main/java/com/example/paris_janitor_api/infrastructure/doc/Swagger.java
@@ -4,11 +4,13 @@ package com.example.paris_janitor_api.infrastructure.doc;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SwaggerConfig {
+public class Swagger {
 
     @Bean
     public OpenAPI customOpenAPI(){
@@ -22,5 +24,22 @@ public class SwaggerConfig {
                                 .email("https://github.com/Jean-6/paris-janitor-spring-boot")
                         )
                 );
+    }
+
+    @Bean
+    public GroupedOpenApi apiGroup(){
+        return GroupedOpenApi.builder()
+                .group("default")
+                .packagesToScan("com.example.paris_janitor_api")
+                .build();
+    }
+
+    @Bean
+    OpenApiCustomizer openApiCustomizer(){
+        return OpenApi -> OpenApi.getPaths().forEach((path,pathItem) ->{
+            pathItem.readOperations().forEach(operation -> {
+                operation.addTagsItem("Custom Tag");
+            });
+        });
     }
 }
