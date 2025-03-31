886fc46805e964074b0791e0a5b9c5594b9b70b2
Jean <oka.jeanjaures@gmail.com>
2025-03-31 02:14:30 +0200

    refactor: config logger

--- src/main/java/com/example/paris_janitor_api/ParisJanitorApiApplication.java
+++ src/main/java/com/example/paris_janitor_api/ParisJanitorApiApplication.java
@@ -1,13 +1,20 @@
 package com.example.paris_janitor_api;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
-
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.example.paris_janitor_api")
 public class ParisJanitorApiApplication {
+
+	static Logger logger = LoggerFactory.getLogger(ParisJanitorApiApplication.class);
+
 	public static void main(String[] args) {
+
+		logger.info("paris janitor api application started");
+
 		SpringApplication.run(ParisJanitorApiApplication.class, args);
 	}
 
